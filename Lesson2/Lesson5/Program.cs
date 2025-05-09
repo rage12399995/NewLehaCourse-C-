d2968d152b29f379cb6a03e7179323d7a36dbe67
Your Name <you@example.com>
2025-05-09 13:36:59 +0300

    lesson changes

---
+++ Lesson2/Lesson5/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Запрос у пользователя двух булевых значений : ");
+        bool dog = Convert.ToBoolean(Console.ReadLine());
+        bool cat = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("bigdog\tlittle cat\tbigdog&little cat\tbigdog|little cat\tbigdog^little cat\t!bigdog");
+        Console.WriteLine($"{dog}\t{cat}\t{dog && cat}\t{dog || cat} \t {dog ^ cat}\t{!dog}");
+
+
+    }
+
+    
+}
