379a9bfbf2dd369e6058bee3caa9dc46daa7840b
Your Name <you@example.com>
2025-05-13 21:33:50 +0300

    Lessons are Finish

---
+++ Lesson4.4/Lesson4.4/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Enter a symbol from the keyboard: W, S, A, D ");
+       
+        var text = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        switch (text)
+    {
+        case 'W':
+                Console.WriteLine("Enter from keyboard W");
+            break;
+        case 'S':
+                Console.WriteLine("Enter from keyboard S");
+                break;
+        case 'A':
+                Console.WriteLine("Enter from keyboard A");
+            break;
+        case 'D':
+                Console.WriteLine("Enter from keyboard D");
+                break;
+            default:
+                Console.WriteLine("This is something else");
+                break;
+    }
+        Console.ReadLine();
+    }
+}
