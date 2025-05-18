11492511718f1e8cc6643bd4794a979c4c03a753
Your Name <you@example.com>
2025-05-18 18:39:05 +0300

    Lessons are Finish

---
+++ Lesson5/Lesson5/Book.cs
@@ -0,0 +1,40 @@
+﻿public class Book
+{
+    public string title;
+
+    public string author;
+
+    public int pages;
+
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+    public int Pages
+    {
+        get { return pages; }
+        set { pages = value >= 0 ? value : 0; } 
+    }
+    public Book()
+    {
+        Title = "Unknown";
+        Author = "Unknown";
+        Pages = 0;
+    }      
+    public Book(string title, string author, int pages)
+    {
+        Title = title;
+        Author = author;
+        Pages = pages;
+    }       
+    public string GetDescription()
+    {
+        return $"Название: {Title}, Автор: {Author}, Страниц: {Pages}";
+    }
+}
