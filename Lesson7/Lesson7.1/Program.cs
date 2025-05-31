06ca594103291cecf1726b2db97840b692b3e2c1
Your Name <you@example.com>
2025-05-31 13:39:44 +0300

    Lessons are Finish

---
+++ Lesson7/Lesson7.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}          
+        };
+
+        int dog = matrix.GetLength(0);
+        int cat = matrix.GetLength(1);
+
+
+        for (int i = 0; i < dog; i++)
+        {
+            int max = matrix[i, 0];
+
+            for (int j = 1; j < cat; j++)
+            {
+                if (matrix[i, j] > max)
+                {
+                    max = matrix[i, j];
+                }
+                
+            }
+            Console.WriteLine($"значения максимального элемента {i + 1}: {max}");
+        }
+        
+    }
+}
---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Введите количество элементов массива:\t");
+
+int elementsCount = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine($"\nВведите  элементов массива под индексом {i}:\t ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nВывод массива:");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+}
+
+
+}
+}
