cfc07df14f52c2e8963449bb6901b60d1528407b
Your Name <you@example.com>
2025-06-14 21:19:52 +0300

    Lessons are Finish

---
+++ TESTLesson1/TESTLesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TESTLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int[,,] mas = { { { 1, 2 },{ 3, 4 } },
+
+            { { 4, 5 }, { 6, 7 } },
+            { { 7, 8 }, { 9, 10 } },
+            { { 10, 11 }, { 12, 13 } }
+
+        };
+        }
+    }
+}
