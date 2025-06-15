ecd543a9d8bec79ad7fcd38deef41af00b020a8e
Your Name <you@example.com>
2025-06-15 20:47:52 +0300

    TESTLesson2

---
+++ TESTLesson2/TESTLesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+       
+        MyString str1 = new MyString();                
+        MyString str2 = new MyString("What a wonderful day!");   
+        MyString str3 = new MyString(new char[] { 'a', 'b', 'c' }); 
+        MyString str4 = new MyString(str2);             
+
+        Console.WriteLine("До реверса:");
+        Console.WriteLine(str2.ToString());
+  
+        str2.Reverse();
+
+        Console.WriteLine("После реверса:");
+        Console.WriteLine(str2.ToString());
+    }
+}
