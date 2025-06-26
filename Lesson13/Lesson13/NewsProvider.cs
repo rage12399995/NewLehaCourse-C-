f44c8c897c8f91e91de94a45e4439665fee044ef
Your Name <you@example.com>
2025-06-26 20:17:09 +0300

    Lessons are Finish

---
+++ Lesson13/Lesson13/NewsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13
+{
+    internal class NewsProvider
+    {
+        public string News { get; set; }
+        public string Weather { get; set; }
+        public string Sport { get; set; }
+        public string Incidents { get; set; }
+        public string Humor { get; set; }
+    }
+}
---
+++ Lesson13/Lesson13/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
