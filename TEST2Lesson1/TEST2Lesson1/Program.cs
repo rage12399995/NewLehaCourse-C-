aa1c2a656b9a2a9c5b332aeb4682560ffda0a476
Your Name <you@example.com>
2025-07-01 21:20:23 +0300

    Lessons are Finish

---
+++ TEST2Lesson1/TEST2Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+static void Main(string[] args)
+{
+        int[,] matrix = new int[4, 4]
+
+        {
+            {1,20,30,45},
+            {10,1,15,40},
+            {45,33,5,12},
+            {22,33,50,1}
+        };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();           
+        }
+         
+}
+}
+
---
+++ TEST2Lesson1/TEST2Lesson2/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string result = number.ToWordString();
+        Console.WriteLine($"Результат: {result}");
+    }
+}
+
+public static class IntExtensions
+{
+    public static string ToWordString(this int number)
+    {
+        string[] words = { "ноль", "один", "два", "три", "четыре",
+                          "пять", "шесть", "семь", "восемь", "девять" };
+
+        string result = "";
+        string numStr = number.ToString();
+
+        foreach (char c in numStr)
+        {
+            if (char.IsDigit(c))
+            {
+                int digit = c - '0';
+                result += words[digit] + " ";
+            }
+        }
+
+        return result.Trim();
+    }
+}
+
+
