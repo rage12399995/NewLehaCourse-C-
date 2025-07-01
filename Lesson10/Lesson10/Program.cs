85fcda17aacec4a8a2040b883184278bb7c62fcd
Your Name <you@example.com>
2025-07-01 21:20:53 +0300

    TestLessons are Finish

---
+++ Lesson10/Lesson10/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lesson10;
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var p1 = new Person { Name = "Leha", Age = 30 };
+        var p2 = new Person { Name = "Alex", Age = 33 };
+        var p3 = new Person { Name = "Alena", Age = 30 };
+
+        Console.WriteLine(p1);
+
+        Console.WriteLine(p1.Equals(p2));
+
+        Console.WriteLine(p3.Equals(p1));
+
+        Console.WriteLine(p3 == p1);
+    }
+}
--- Lesson13/Lesson13/NewsProvider.cs
+++ Lesson13/Lesson13/NewsProvider.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace Lesson13
 {
-    internal class NewsProvider
+    public class NewsProvider
     {
+ 
         public string News { get; set; }
         public string Weather { get; set; }
         public string Sport { get; set; }
         public string Incidents { get; set; }
         public string Humor { get; set; }
+        public object ProviderNews { get; }
+        public List<string> Recipients { get; set; }
+        public object? newsProvider { get; private set; }
+
+        public NewsProvider(string providernews)
+        {
+            ProviderNews = newsProvider;
+
+            Recipients = new List<string>();
+        }
     }
 }
